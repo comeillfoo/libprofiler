de22ee03f8d0094d876f7efd8d79c60bdc46bd78
come_ill_foo <rayveleaveme34@inbox.ru>
2023-02-25 12:36:00 +0000

    init: profiler from the DLL

---
+++ DllMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibProfiler;
+
+public class DllMain {
+    [UnmanagedCallersOnly(EntryPoint = "DllGetClassObject")]
+    public static unsafe int DllGetClassObject(Guid* rclsid, Guid* riid, IntPtr* ppv) {
+        Console.WriteLine("Hello from the profiling API");
+
+        return 0;
+    }
+}
